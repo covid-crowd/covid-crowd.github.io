d2bef24a03288d0d0fd255fb26dad37077f877b3
jritch <jacob.w.k.ritchie@gmail.com>
2020-03-29 17:41:40 -0400

    added d3 map

--- graphs.js
+++ graphs.js
@@ -1,34 +1,90 @@
   // global variables used across all charts
-  var margin = {top: 30, right: 30, bottom: 60, left: 60};
-  var width = Math.min(600,document.getElementsByClassName("container")[0].offsetWidth - 10);
+  var padding = 20;
+  var width = Math.min(900,document.getElementsByClassName("container")[0].offsetWidth - 10);
   var height = 300;
-  var start_day = new Date("2020-01-15");
 
-  function update_dates(data) {
-    data.map( function(x) {
-        let d = new Date(); 
-        d.setTime(start_day.getTime() + 86400000 * x.day); 
-        x.day = d; return x;
-    });
-  }
-
-  function drawGraph(data,svg_id,intervention_dates,title) {
-    update_dates(data)
-    console.log(data)
-    const svg = d3.select("#" + svg_id)
-    
-    var g =  svg.append("g")
-              .attr("transform",
-                    "translate(" + margin.left + "," + margin.top + ")")
-  }
-
-  function make_plot(data_file, svg_id, intervention_dates,title) {
-            d3.csv(data_file).then(
-                function(data) {
-                    drawGraph(data,svg_id,intervention_dates,title)
-                });   
+  function drawGraph(values) {    
+    var path = d3.geoPath()
+    var svg = d3.select("#mapcontainer")
+    const color = function (x) {
+      if (x == 0){ return "none"; }
+      if (x == 1){ return "#eaab00"; }
+      if (x == 2){ return "#175e54"; }
     }
-    
 
+    var data = values[0]
+    var us = values[1]
+
+    // code from Bostock's d3 documentation on Observable  
+    // URL: 
+
+    svg_string = `<svg viewBox="0 0 ${width} 610">
+  <g fill="none" stroke="#000" stroke-linejoin="round" stroke-linecap="round">
+    <path stroke="#aaa" stroke-width="0.5" d="${path(topojson.mesh(us, us.objects.counties, (a, b) => a !== b && (a.id / 1000 | 0) === (b.id / 1000 | 0)))}"></path>
+    <path stroke-width="0.5" d="${path(topojson.mesh(us, us.objects.states, (a, b) => a !== b))}"></path>
+    <path d="${path(topojson.feature(us, us.objects.nation))}"></path>
+  </g>
+</svg>`
   
-make_plot("","chart_1",[],"Crowdsourced Intervention Data")
+    svg.html(svg_string)
+
+    svg = d3.select("svg")
+
+
+    counties = topojson.feature(us, us.objects.counties).features
+
+    counties.forEach(function (item) {
+        item.data = data.find(function (d) { return d.id === item.id })
+    })
+
+
+    // code to select a state and outline it in green 
+    svg.append("g")
+    .attr("opacity", "0")
+    .selectAll("path")
+    .data(topojson.feature(us, us.objects.states).features)
+    .enter().append("path")
+      .attr("d", path)
+      .on("click", d => {
+        outline.attr("d", path(d));
+        console.log(d.id)
+        if (d.id == "36") {
+          d3.select("#california-info").attr("style","display:none;")
+          d3.select("#new-york-info").attr("style","display:block;")
+          d3.select("#santa-clara-info").attr("style","display:none;")
+        }
+        if (d.id == "06") {
+          d3.select("#california-info").attr("style","display:block;")
+          d3.select("#new-york-info").attr("style","display:none;")
+          d3.select("#santa-clara-info").attr("style","display:none;")
+        }
+      });
+
+      const outline = svg.append("path")
+      .attr("fill", "none")
+      .attr("stroke", "#175e54")
+      .attr("stroke-width", "3")
+      .attr("stroke-linejoin", "round")
+      .attr("pointer-events", "none");
+
+      d3.select("#santa-clara-link").on("click", () => {d3.select("#santa-clara-info").attr("style","display:block;")})
+
+
+    d3.select("svg").append("g")
+    .selectAll("path")
+    .data(counties)
+    .join("path")      
+      .attr("fill", d => color(d.data.status))
+      .attr("d", path)
+      .on("click", d => console.log(d.id))
+
+  }
+
+var promises = [];
+
+promises.push(d3.csv('d3-data/county_data.csv'))
+promises.push(d3.json('d3-data/counties-albers-10m.json'))
+
+Promise.all(promises).then(function(values) {
+    drawGraph(values)
+});
