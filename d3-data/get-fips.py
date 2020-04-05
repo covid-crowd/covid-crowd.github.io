cfb30564ce16b9f52fd99adb0d15537b112c8efc
jritch <jacob.w.k.ritchie@gmail.com>
2020-04-05 14:26:36 -0400

    push new data

--- d3-data/get-fips.py
+++ d3-data/get-fips.py
@@ -3,7 +3,7 @@ intervention_df = pd.read_csv("intervention_data.csv",dtype=str)
 county_df = pd.read_csv("county_data.csv",dtype=str)
 df = pd.merge(intervention_df,county_df, how="left", left_on=["location_1","location_2"] , right_on=["STNAME","CTYNAME"])
 df['fips']  = df['id']
-df = df[intervention_df.columns.values]
+df = df[list(intervention_df.columns.values) + ["fips"]]
 print(df)
 df.to_csv("intervention_data.csv", index=False)
 
