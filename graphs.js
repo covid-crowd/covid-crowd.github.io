1a10e8cc0535b01b3e0ea9fc9a642c0fa4b00917
jritch <jacob.w.k.ritchie@gmail.com>
2020-04-06 00:25:19 -0400

    make widget nice

--- graphs.js
+++ graphs.js
@@ -2,19 +2,193 @@
 var padding = 20;
 var width = 975;
 var height = 610;
+var add_county_info;
 
 function drawGraph(values) {    
   var path = d3.geoPath();
+  
   const color = function (x) {
     if (x == 0){ return "none"; }
     if (x == 1){ return "#eaab00"; }
     if (x == 2){ return "#175e54"; }
   }
+  
+  Date.prototype.to_string = function ()  {
+    var options = { year: 'numeric', month: 'long', day: 'numeric' };  
+    return this.toLocaleString('en-US', options)
+  }
 
-  add_state_info = function(d,counties_in_state,state)  {
+  function get_business_string (county) {
+    data = county.intervention_data
+
+    return get_question_string("Non-essential businesses", data.business_closed, 
+      data.business_closed_date, data.business_closed_url, data.business_open,
+       data.business_open_date, data.business_open_url)
+  }
+
+  function get_school_string(county) {
+    data = county.intervention_data
+
+    return get_question_string("K-12 schools", data.school_closed, 
+      data.school_closed_date, data.school_closed_url, data.school_open,
+       data.school_open_date, data.school_open_url)
+  }
+
+  function get_college_string(county) {
+    data = county.intervention_data
+
+    return get_question_string("Colleges and/or universities", data.school_closed, 
+      data.school_closed_date, data.school_closed_url, data.school_open,
+       data.school_open_date, data.school_open_url)
+  }
+
+  function get_question_string(plural_entity_name, closed, closed_date, closed_url, open, open_date, open_url) {
+    if (closed != "Yes")
+    {
+      question_string = "<div>" + plural_entity_name + " remain open."
+    }
+    else {
+      question_string = "<div>" + plural_entity_name + " were closed"
+      if (closed_date)
+      {
+        question_string += " on " + new Date(closed_date).to_string() + ".";
+      }
+      else {
+         question_string += ", no date provided."
+      }
+    }
+    if (closed_url) {
+      question_string += ' (<a href="' + closed_url + '">Source</a>)'
+    }
+    else {
+      question_string += " (Missing source)"
+    }
+    if (open == "Yes") {
+      question_string += " " + plural_entity_name + " were reopened"  
+      if (open_date)
+      {
+        question_string += " on " + new Date(open_date).to_string() + ".";
+      }
+      else {
+         question_string += ", no date provided."
+      }
+      if (open_url) {
+        question_string += ' (<a href="' + open_url + '">Source</a>)'
+      }
+      else {
+        question_string += " (Missing source)"
+      }
+    }
+    question_string += "</div>"
+    return question_string
+  }
+
+  function get_religion_string(county) {
+    data = county.intervention_data
+    if (data.religion_closed != "Yes")
+    {
+      question_string = "<div>Religious gatherings continue to take place"
+    }
+    else {
+      question_string = "<div>Some or all religious gatherings ceased"
+      if (data.religion_closed_date)
+      {
+        question_string += " on " + new Date(data.religion_closed_date).to_string() + ".";
+      }
+      else {
+         question_string += ", no date provided."
+      }
+      
+    }
+    if (data.religion_closed_url) {
+        question_string += ' (<a href="' + data.religion_closed_url + '">Source</a>)'
+    }
+    else {
+        question_string += " (Missing source)"
+    }
+    if (data.religion_open == "Yes") {
+      question_string += "Religious gatherings resumed"  
+      if (data.religion_open_date)
+      {
+        question_string += " on " + new Date(data.religion_open_date).to_string() + ".";
+      }
+      else {
+         question_string += ", no date provided."
+      }
+      if (data.religion_open_url) {
+        question_string += ' (<a href="' + data.religion_open_url + '">Source</a>)'
+      }
+      else {
+        question_string += " (Missing source)"
+      }
+    }
+    question_string += "</div>"
+    return question_string
+  }
+
+  function get_lockdown_string(county) {
+    data = county.intervention_data
+    if (data.lockdown_closed != "Yes")
+    {
+      question_string = "<div>No order to stay at home has been put in place."
+    }
+    else {
+      question_string = "<div>An order to stay at home has been put in place"
+      if (data.lockdown_closed_date)
+      {
+        question_string += " on " + new Date(data.lockdown_closed_date).to_string() + ".";
+      }
+      else {
+         question_string += ", no date provided."
+      }
+      
+    }
+    if (data.lockdown_closed_url) {
+      question_string += ' (<a href="' + data.lockdown_closed_url + '">Source</a>)'
+    }
+    else {
+      question_string += " (Missing source)"
+    }
+    if (data.lockdown_open == "Yes") {
+      question_string += "The was lifted"  
+      if (data.lockdown_open_date)
+      {
+        question_string += " on " + new Date(data.lockdown_open_date).to_string() + ".";
+      }
+      else {
+         question_string += ", no date provided."
+      }
+      if (data.lockdown_open_url) {
+        question_string += ' (<a href="' + data.lockdown_open_url + '">Source</a>)'
+      }
+      else {
+        question_string += " (Missing source)"
+      }
+    }
+    question_string += "</div>"
+    return question_string
+  }
+
+  add_county_info = function(id) {
+    console.log(id)
+    console.log(counties);
+    county = counties.find(function (d) { return d.id === id });
+    console.log(county);
+    var div_content = `<div class="pill main-pill"><h4> ${county.data.CTYNAME} </h4></div><br> <div class="county-text">` + 
+      get_business_string(county) + get_school_string(county) + get_college_string(county)  + 
+      get_religion_string(county) + get_lockdown_string(county) + "</div>"
+
+    document.getElementById("county-info").innerHTML = div_content;
+    document.getElementById("state-info").setAttribute("style","border-radius: 0px");
+  }
+
+  const add_state_info = function(d,counties_in_state,state)  {
 
     //`<p><b class="status-2">We have complete data from these ${} counties: ${county_names}`
     
+    document.getElementById("county-info").innerHTML = "";
+    document.getElementById("state-info").setAttribute("style","");
+
     counties_with_intervention_data = counties_in_state.filter(function(item) {
         return item.intervention_data != undefined;
     })
@@ -22,8 +196,8 @@ function drawGraph(values) {
     console.log(counties_with_intervention_data)
 
     county_names = counties_with_intervention_data.map(function (item) {
-        return `<a onclick="add_county_info(${item.id})">${item.properties.name}</a>`
-    }).join(", ")
+        return `<div class="pill"><a onclick="add_county_info('${item.id}')">${item.properties.name}</a></div>`
+    }).join(" ")
 
     var partial_counties, no_data_counties;
     num_partial_counties = counties_with_intervention_data.length;
@@ -34,12 +208,12 @@ function drawGraph(values) {
     } 
     else if (num_partial_counties == 1) {
       partial_counties = `<p><b class="status-1">We have partial data from one ` + 
-            `county, but need more info from residents! </b> `+
+            `county, but need more info from residents! </b><br> `+
             `${county_names}</p>`  
     }
     else {
         partial_counties = `<p><b class="status-1">We have partial data from these ` + 
-            `${num_partial_counties} counties, but need more info from residents! </b>`+
+            `${num_partial_counties} counties, but need more info from residents! </b><br>`+
             `${county_names}</p>`  
     }
 
@@ -58,17 +232,14 @@ function drawGraph(values) {
         more counties in this state, anyone can volunteer!</b></p>`
     }
 
-    var div_content = `<h4> ${state} </h4>                
+    var div_content = `<div class="pill main-pill"><h4> ${state} </h4></div>                
                       ${partial_counties}  
                       ${no_data_counties}
                       `
     document.getElementById("state-info").innerHTML = div_content;
   }
 
-  add_county_info = function(argument) {
-    var div_content = `<h4> </h4>`
-    document.getElementById("county-info").innerHTML = div_content;
-  }
+  
 
 
   var county_data = values[0];
@@ -140,6 +311,7 @@ function drawGraph(values) {
   california_counties = counties.filter(function (item) { return "06" === item.id.slice(0,2); });
   outline.attr("d",path(california));
   add_state_info(california,california_counties,"California");
+  add_county_info("06075")
 
   d3.select("#santa-clara-link").on("click", () => {d3.select("#santa-clara-info").attr("style","display:block;")})
 
